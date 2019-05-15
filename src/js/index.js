db4519fe4e380c38883582f1e0e9a6e11379c2bb
Adam Borys <hipowolemia@gmail.com>
2019-05-15 03:04:49 +0200

    Show more button functional

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,34 @@
 const Http = new XMLHttpRequest();
 const url = 'http://www.splashbase.co/api/v1/images/search?query=tree';
 
-let imageList;
+let gallery;
+let loadMoreBtn;
+let imagesDataArr;
+
+function loadMore() {
+  console.log(imagesDataArr);
+  const nextArr = imagesDataArr.splice(0, 10);
+  const imageList = document.createElement('UL');
+  imageList.className = 'gallery__images';
+  const len = nextArr.length;
+  for (let i = 0; i < len; i += 1) {
+    const listItem = document.createElement('LI');
+    const newFigure = document.createElement('FIGURE');
+    const newImage = new Image();
+    newImage.src = nextArr[i].url;
+    newFigure.appendChild(newImage);
+    listItem.appendChild(newFigure);
+    imageList.appendChild(listItem);
+  }
+  gallery.appendChild(imageList);
+  console.log(imagesDataArr);
+}
+
+window.onload = () => {
+  gallery = document.getElementById('gallery');
+  loadMoreBtn = document.getElementById('more-btn');
+  Http.send();
+};
 
 Http.open('GET', url);
 
@@ -17,20 +44,8 @@ Http.onload = () => {
 Http.onreadystatechange = function loadImages() {
   console.log(`${this.readyState} ${this.status}`);
   if (this.readyState === 4 && this.status === 200) {
-    const imagesDataArr = JSON.parse(this.responseText).images;
-    for (let i = 0; i < 10; i += 1) {
-      const listItem = document.createElement('LI');
-      const newFigure = document.createElement('FIGURE');
-      const newImage = new Image();
-      newImage.src = imagesDataArr[i].url;
-      newFigure.appendChild(newImage);
-      listItem.appendChild(newFigure);
-      imageList.appendChild(listItem);
-    }
+    imagesDataArr = JSON.parse(this.responseText).images;
+    loadMoreBtn.onclick = loadMore;
+    loadMore();
   }
-};
-
-window.onload = () => {
-  imageList = document.getElementById('images');
-  Http.send();
 };
